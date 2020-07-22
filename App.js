ec95f6c91c77362f14025a428ae9d1c25c3a4cdf
prajwalr308 <prajwalr.thedeveloper@gmail.com>
2020-07-22 18:18:39 +0530

    learning usestate in react native

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View ,TextInput } from 'react-native';
 
 export default function App() {
+  const [name,setName]=useState('john');
+  const [age,setAge]=useState('18');
+  function newName(val)
+ {
+    setName(val)
+  }
+  function newAge(val)
+  {
+     setAge(val)
+   }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Enter Name: </Text>
+     
+      <TextInput
+      placeholder='e.g. john doe' 
+      style={styles.input}
+      onChangeText={newName}
+      />
+      <Text>Enter age: </Text>
+       <TextInput
+      placeholder='e.g. 18' 
+      style={styles.input}
+      onChangeText={newName}
+      />
+      <Text>name:{name},age:{age}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +41,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderWidth:1,
+    borderColor:'blue',
+    padding:8,
+    margin:10,
+    width:200
+
+  }
 });
